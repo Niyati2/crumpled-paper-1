6d87e943953d76f896034635684760f89bfa339b
niyati <71449171+Niyati2@users.noreply.github.com>
2020-11-02 20:57:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,36 +7,25 @@ const Render = Matter.Render;
 var dustbinObj, paperObject,groundObject	
 var world;
 
-
 function setup() {
 	createCanvas(1600, 700);
 	rectMode(CENTER);
-
-
-	engine = Engine.create();
+engine = Engine.create();
 	world = engine.world;
 	dustbinObj=new dustbin(1200,650);
 	paperObject=new paper(200,450,40);
 	groundObject=new ground(width/2,670,width,20);
-	//Create a Ground
 	
-
 	var render = Render.create({
 	  element: document.body,
 	  engine: engine,
 	  options: {
 	    width: 1200,
 	    height: 700,
-	  
 	  }
-	});
-
-	Engine.run(engine);
-	//Render.run(render);
-  
+	})
+	Engine.run(engine); 
 }
-
-
 function draw() {
   rectMode(CENTER);
   background(0);
@@ -44,13 +33,7 @@ function draw() {
   dustbinObj.display();
   paperObject.display();
   groundObject.display();
-  
- 
-  
-  
- 
 }
-
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
@@ -58,8 +41,3 @@ function keyPressed() {
     
   	}
 }
-
-
-
-
-
